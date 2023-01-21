e043f6820fa6b0aa3a3be700c3d3d480b2186ffc
Ajay Johar <ajayjohar3136@gmail.com>
2023-01-20 23:16:40 -0500

    created application.yml file for configuration

--- src/main/java/com/udemyCourse/simpleAPI/CurrencyServiceConfiguration.java
+++ src/main/java/com/udemyCourse/simpleAPI/CurrencyServiceConfiguration.java
@@ -1,15 +1,31 @@
 package com.udemyCourse.simpleAPI;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "currency-service")
+@ConfigurationProperties(prefix = "application")
 @Component
 public class CurrencyServiceConfiguration {
 	
+	@Value("${currency-service.url}")
 	private String url;
+	
+	@Value("${currency-service.username}")
 	private String username;
+	
+	@Value("${currency-service.key}")
 	private String key;
+	
+	@Value("${myapp.property1}")
+	private String property1;
+	
+	public String getProperty1() {
+		return property1;
+	}
+	public void setProperty1(String property1) {
+		this.property1 = property1;
+	}
 	public String getUrl() {
 		return url;
 	}
@@ -29,6 +45,4 @@ public class CurrencyServiceConfiguration {
 		this.key = key;
 	}
 	
-	
-
 }
